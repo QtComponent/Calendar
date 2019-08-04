a8df0ecdf117ad889f77e8a2dd380d764cdd3d5a
Qt君 <2088201923@qq.com>
2019-08-04 17:25:47 +0800

    上传示例程序

---
+++ Test/main.cpp
@@ -0,0 +1,28 @@
+﻿#include "../LunarCalendar.h"
+#include <QApplication>
+
+#if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
+#include <QDeclarativeView>
+#else
+#include <QQuickView>
+#endif
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    qmlRegisterType<LunarCalendar>("LunarCalendar",
+                                   1,
+                                   0,
+                                   "LunarCalendar");
+
+#if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
+    QDeclarativeView viewer;
+#else
+    QQuickView viewer;
+#endif
+
+    viewer.setTitle(APP_NAME + QString("(Qt君)"));
+    viewer.setSource(QUrl::fromLocalFile(QLatin1String("../Calender.qml")));
+    viewer.show();
+    return app.exec();
+}
